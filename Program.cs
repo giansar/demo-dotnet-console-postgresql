a2016c0d43cb79ec8747c95f5187dbd639860620
Giansar Pratiknya <gpratiknya@gmail.com>
2021-02-12 23:44:02 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace demo_dotnet_console_postgresql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+            String connectionString = configuration.GetConnectionString("postgresql");
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Postgresql version = " + connection.ServerVersion);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
